d2716ebf00375808afeca53036e1cf400e9a553f
KevinMaganaM <magana.rodrigo31@gmail.com>
2024-06-20 18:09:05 -0700

    add circle class

---
+++ main.py
@@ -0,0 +1,15 @@
+class Circle:
+
+    
+    def calculate_area(self):
+        print(3.14*self.radius**2)
+        
+    def calculate_perimeter(self):
+        print(2*3.14*self.radius)
+
+    def __init__(self,radius):
+        self.radius = radius
+
+circle1 = Circle(3)
+circle1.calculate_area()
+circle1.calculate_perimeter()
