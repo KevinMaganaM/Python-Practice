06bbfb45497c617684b65d7d0f71a22e2fc47ea0
KevinMaganaM <magana.rodrigo31@gmail.com>
2024-08-22 15:19:53 -0700

    15 python basic exercises from Pynative

--- PYnative_exercises.py
+++ PYnative_exercises.py
@@ -84,3 +84,111 @@ for r in range(1, rows + 1):
     for c in range(1, r + 1):
         print(c, end=" ")
     print(" ")
+
+print("-----------------------------------------------------------")
+
+for num in range(1,6):
+    for i in range(num):
+        print(num, end=" ")
+    print(" ")
+
+print("-----------------------------------------------------------")
+
+number = int(input("write a number: "))
+palindrome = "No"
+original_number = number
+
+reverse_number = 0
+while number > 0:
+    reminder = number % 10
+    reverse_number = (reverse_number * 10) + reminder
+    number = number // 10
+
+
+if original_number == reverse_number:
+    palindrome = "Yes"
+    print(f"{palindrome}. Given number is palindrome")
+else:
+    print(f"{palindrome}. Given number isn't palindrome")
+
+print("------------------------------------------------------------")
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+list3 = []
+
+for num in list1:
+    if num % 2 != 0:
+        list3.append(num)
+
+for num in list2:
+    if num % 2 == 0:
+        list3.append(num)
+
+print(f"result list: {list3}")
+
+print("--------------------------------------------------------------")
+
+number = int(input("write a number: "))
+
+while number > 0:
+    # get the last digit
+    reminder = number % 10
+    # remove the last digit and repeat loop
+    number = number // 10
+    print(reminder, end=" ")
+
+print("----------------------------------------------------------------")
+
+income = 45000
+tax_payable = 0
+print("Given income", income)
+
+if income <= 10000:
+    tax_payable = 0
+elif income <= 20000:
+    # no tax on first 10,000
+    x = income - 10000
+    # 10% tax
+    tax_payable = x * 10 / 100
+else:
+    # first 10,000
+    tax_payable = 0
+
+    # next 10,000 10% tax
+    tax_payable = 10000 * 10 / 100
+
+    # remaining 20%tax
+    tax_payable += (income - 20000) * 20 / 100
+
+print("Total tax to pay is", tax_payable)
+
+print("------------------------------------------------------")
+
+for r in range(1, 11):
+    for c in range(1,11 ):
+        print(r * c, end=" ")
+    print(" ")
+
+print("--------------------------------------------------------")
+
+for r in range(5, 0, -1):
+    for c in range(1, r + 1):
+        print("*", end=" ")
+    print(" ")
+
+print("----------------------------------------------------------")
+
+base = int(input("write the base: "))
+exp = int(input("write the power: "))
+
+def exponent(base, exp):
+    print(f"Base= {base}")
+    print(f"Exponent= {exp}")
+
+    base_1 = base
+    for i in range(exp - 1):
+        base = base * base_1
+    print(f'{base_1} to the power of {exp}: {base}')
+
+exponent(base,exp)
